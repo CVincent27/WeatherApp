565c324307bedb9436501839c376860a34d7bb33
Vinchenzer <cardon.vincent@hotmail.fr>
2023-09-04 15:27:34 +0200

    test api

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,14 +23,92 @@ namespace WeatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string baseApi = "http://api.weatherapi.com/v1/current.json?key=ab6a6db4fda04775877113817230409%20&q=";
+        public string city = "Lille";
+        public string JsonString;
+
+
+        // Partie fonction main
         public MainWindow()
         {
             InitializeComponent();
+            SetHeaderImg();
+            SetDay();
+            GetApiResponse();
+            SetUiInfos();
+
+
+
         }
+
+        // Partie fonctions
+
+        // Fonction API/JSON
+        public void GetApiResponse()
+        {
+            using(WebClient wc  = new WebClient())
+            {
+                // Télecharger du JSON depuis l'API
+                JsonString = wc.DownloadString(baseApi + city);
+                // Encodage UTF8 de la rép
+                byte[] bytes = Encoding.UTF8.GetBytes(JsonString);
+                JsonString = Encoding.UTF8.GetString(bytes);
+            }
+        }
+
+        public void SetUiInfos()
+        {
+            JObject o = JObject.Parse(JsonString);
+            MessageBox.Show(JsonString);
+            
+        }
+
+            // Fonction de base
+        // Gérer la date 
+        public void SetDay()
+        {
+            // Premiere lettre en MAJUSCULE
+            dateTxt.Content = CapitalizeStr(DateTime.Now.ToString("dddd dd MMMM yyyy"));
+
+            forecast1.Content = CapitalizeStr(DateTime.Now.AddDays(1).ToString("ddd"));
+            forecast2.Content = CapitalizeStr(DateTime.Now.AddDays(2).ToString("ddd"));
+            forecast3.Content = CapitalizeStr(DateTime.Now.AddDays(3).ToString("ddd"));
+        }
+
+        public string CapitalizeStr(string str)
+        {
+            string strFormat = str;
+            strFormat = char.ToUpper(str[0]) + str.Substring(1);
+            return strFormat;
+        }
+
+        public void SetHeaderImg()
+        {
+            string meteo = "pluie";
+            if (meteo == "pluie")
+            {
+                // Modifier l'image du header
+                headerImg.Source = new BitmapImage(new Uri(@"/sunset.jpg", UriKind.Relative));
+            }
+            else
+            {
+                // On ne modifie pas l'image 
+            }
+        }
+
+        // Partie gestion des evenements
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
+            city = cityTxt.Text;
+            cityTitle.Content = city;
+            GetApiResponse();
+            SetUiInfos();
+        }
 
+        private void btnInfos_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Développé par Vincent CARDON", "Crédits", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
